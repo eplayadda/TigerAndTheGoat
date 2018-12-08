da5b1b1683a548dd9a5333c25744189ab2ee0f35
DESKTOP-L8QPA25\chandan <chandanroy565@gmail.com>
2018-12-09 00:08:55 +0530

    no message

--- Assets/TG_Fight/Scripts/Manager/UIManager.cs
+++ Assets/TG_Fight/Scripts/Manager/UIManager.cs
@@ -100,7 +100,7 @@ public class UIManager : MonoBehaviour
 
 	public void FriendGameOver ()
 	{
-		if(GameManager.instance.currGameStatus == eGameStatus.gameover)
+		if(GameManager.instance.currGameStatus != eGameStatus.gameover)
 			OnDicliend ();
 		Debug.Log ("Friend Game Quit");
 	}
@@ -109,14 +109,15 @@ public class UIManager : MonoBehaviour
 	{
 		
 		Invoke ("GameOverInvoke", 1f);
-		if (GameManager.instance.currGameMode == eGameMode.vServerMulltiPlayer) {
-			ConnectionManager.Instance.OnGameOverSendData (ConnectionManager.Instance.myID);
-		}
+
 	}
 
 	void GameOverInvoke ()
 	{
 		gameOverUI.gameObject.SetActive (true);
+		if (GameManager.instance.currGameMode == eGameMode.vServerMulltiPlayer) {
+			ConnectionManager.Instance.OnGameOverSendData (ConnectionManager.Instance.myID);
+		}
 	}
 
 	public void DisableAllUI ()
