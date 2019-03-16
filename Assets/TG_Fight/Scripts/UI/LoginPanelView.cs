db001d6799e6ff8af71c94c353e0ac69230fc1e4
DESKTOP-L8QPA25\chandan <eplayadda@gmail.com>
2019-03-16 23:50:04 +0530

    ads added

---
+++ Assets/TG_Fight/Scripts/UI/LoginPanelView.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoginPanelView : MonoBehaviour {
+
+	void OnEnable()
+	{
+		Debug.Log ("Login Panel Ads Request");
+		AdsHandler.Instance.ShowInterstitialAds();
+	}
+}
